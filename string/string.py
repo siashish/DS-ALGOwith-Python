43fe4ff469498b4ca074eecd763a202b64c5f310
Ashish Singh <ashishsingh21798@gmail.com>
2022-04-21 21:59:50 +0530

    String in python

---
+++ string/string.py
@@ -0,0 +1,42 @@
+# https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+
+# defining string in python
+string = 'hello world!\
+    sdhf'
+print (string)
+
+string = "Hello \"India!\
+svdfdvhds\""
+print(string)
+
+string = '10'
+print([ord(c) for c in string])
+
+
+string = '''Hello UP!
+hello yogi ji!'''
+print(string)
+
+string = """Hello Varanasi!
+            hello Modi ji!"""
+print(string)
+
+# Accessing string in python
+name = 'Ashish'
+print('name = ',name)
+
+print('name[0] = ',name[0])
+print('name[-6] = ',name[-6])
+
+print('name[5] = ',name[5])
+print('name[-1] = ',name[-1])
+
+print(name[1:5])
+print(name[-6:6])
+print(name[-6:5])
+print(name[::2])
+print(name[1:5:-1])
+
+
+
+
