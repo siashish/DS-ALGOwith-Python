e87103d0b100fecff0d170444ae5545f9a54e82b
Ashish Singh <ashishsingh21798@gmail.com>
2022-04-22 21:52:40 +0530

    string in python

--- string/string.py
+++ string/string.py
@@ -38,5 +38,49 @@ print(name[::2])
 print(name[1:5:-1])
 
 
+# print(name[13])
+# print(name[1.5])
+print(name[:13])
+
+
+# updating string
+# it's give error
+# new = 'hello KGF'
+# new[3]= 'L'
+# print(new)
+
+# Concatenation in string
+str1 = 'hello'
+str2 = 'KGF '
+print(str1 +' '+ str2)
+print(str2+str2+str2)
+print(str2*3)
+
+# iterate over string
+count = 0
+for l in 'Hello World':
+    if (l=='l'):
+        count+=1
+print(count,'letters found')
+
+print('a' in 'program')
+print('d' in 'program')
+
+str = "Shital"
+print(len(str))
+
+str = "Heena \n Pooja"
+print(str)
+
+str = "Neha"
+print(list(enumerate(str)))
+for id, ch in enumerate(str):
+    print(id,ch)
+
+print("He say \"what's there?\"")
+print("This is \x61 \n good one")
+print(R"This is \x61 \n good one")
+
+
 
 
