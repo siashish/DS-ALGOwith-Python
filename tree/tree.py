3260cf6aa63c08fe8449d6302e831e7cf1fe80f5
Ashish Singh <ashishsingh21798@gmail.com>
2022-05-11 22:02:43 +0530

    tree data structure in python

---
+++ tree/tree.py
@@ -0,0 +1,13 @@
+# Parent Node
+# Child Node
+# Root Node
+# Degree of a Node
+# Leaf Node/ external Node
+# Inernal Node
+# Depth of the node
+# Height of Node
+# Sibling
+# Ancestor of a Node
+# Desecendent 
+# Level of a Node
+# Subtree
