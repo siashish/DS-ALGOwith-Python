df09a3701ebf2702445cc20e1660705a8b46660a
Ashish Singh <ashishsingh21798@gmail.com>
2022-05-12 21:55:43 +0530

    tree data structure in python

--- tree/tree.py
+++ tree/tree.py
@@ -11,3 +11,45 @@
 # Desecendent 
 # Level of a Node
 # Subtree
+
+# def printPraents(node,adj,parent):
+#     if(parent==0):
+#         print(node,"->Root")
+#     else:
+#         print(node,"->",parent)
+
+#     for
+
+N =7
+Root=1
+
+#Adjestent list
+adj = []
+for i in range(0,N+1):
+    adj.append([])
+
+print(adj)
+
+# tree
+adj[1].append(2)
+adj[1].append(3)
+adj[1].append(4)
+
+adj[2].append(1)
+adj[2].append(5)
+adj[2].append(6)
+
+adj[3].append(1)
+
+adj[4].append(1)
+adj[4].append(7)
+
+adj[5].append(2)
+
+adj[6].append(2)
+
+adj[7].append(4)
+
+print(adj)
+
+
